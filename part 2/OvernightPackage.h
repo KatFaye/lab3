4410c6ab79e7a415174f752868a11a5e6585efe6
Kate Barlock <kbarlock@nd.edu>
2016-02-07 10:30:11 -0500

    Added file skeletons for Part 2

---
+++ part 2/OvernightPackage.h
@@ -0,0 +1,16 @@
+//OvernightPackage class definition
+// Kat Herring and Kate Barlock
+
+class OvernightPackage{
+
+public:
+	OvernightPackage();
+	//redefinte calculateCost to add additional cost to cost per ounce 
+	//Note: use base class's calculateCost function
+
+private:
+	double extraCostPerOunce;
+
+	
+
+};
---
+++ part 2/TwoDayPackage.cpp
@@ -0,0 +1,12 @@
+//Implementation of TwoDayPackage derived class (base class is Package)
+// Kat Herring and Kate Barlock
+
+#include "Package.h"
+#include "TwoDayPackage.h"
+
+TwoDayPackage::TwoDayPackage(){
+
+
+}
+
+//also need refined CalculateCost function (how do you do this with inheritance?)
